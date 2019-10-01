c400e5495ebbbc04d0a98d52d7bd08ed4f0cf2f5
tomasriv2017 <tomasriv2017@gmail.com>
2019-10-01 14:33:25 -0300

    Update (ABM) Concensionaria

--- STP Concensionaria(ABM).sql
+++ STP Concensionaria(ABM).sql
@@ -28,39 +28,39 @@ BEGIN
     select count(*) into cantidadRepetida from concensionaria where cuitConcensionaria = _cuitConcensionaria;
     select count(*) into cantidadPedido from pedido where fk_cuitConcensionaria = _cuitConcensionaria;
     
-    if(cantidadRepetida = 0) then
-		select -1 into nResultado;
+    if(cantidadRepetida > 0) then
+		if(cantidadPedido > 0) then
+			select -1 into nResultado;
+            select "No Se Puede eliminar la concensionaria debido a que tiene uno o mas pedidos" into cMensaje;
+		else
+			select 0 into nResultado;
+            delete from concensionaria where cuitConcensionaria = _cuitConcensionaria;
+		end if;
+	else
+		select -2 into nResultado;
         select "La Concensionaria que quiere eliminar no existe" into cMensaje;
 	end if;
-    
-	if(cantidadRepetida > 0 and cantidadPedido > 0) then
-		select -2 into nResultado;
-        select "La Concensionaria que quiere eliminar no se puede debido a que tiene uno o mas pedidos" into cMensaje;
-	end if;
-	
-    if(cantidadRepetida > 0 and cantidadPedido = 0) then
-		select 0 into nResultado;
-        delete from concensionaria where cuitConcensionaria = _cuitConcensionaria;
-	end if;
-        
 END$$
 DELIMITER ;
 
 
 /*MODIFICACION*/
 DELIMITER $$
-CREATE PROCEDURE modificarConcensionaria( in _cuitConcensionaria varchar(45), in _razonSocial varchar(45) , out cMensaje varchar(100),out nResultado int )
+CREATE PROCEDURE modificarConcensionaria( in _cuitConcensionaria varchar(45) , in nueva_cuitConcensionaria varchar(45), in nueva_razonSocial varchar(45) , out cMensaje varchar(100),out nResultado int )
 BEGIN
 	declare cantidadRepetida int default 0;
     select count(*) into cantidadRepetida from concensionaria where cuitConcensionaria = _cuitConcensionaria;
     
     if(cantidadRepetida > 0) then
-		if(_razonSocial <> (select razonSocial from concensionaria where cuitConcensionaria = _cuitConcensionaria) ) then
-			UPDATE concensionaria set razonSocial = _razonSocial where cuitConcensionaria = _cuitConcensionaria;
+		if( nueva_cuitConcensionaria <> (select cuitConcensionaria from concensionaria where cuitConcensionaria = _cuitConcensionaria) 
+			and nueva_razonSocial <> (select razonSocial from concensionaria where cuitConcensionaria = _cuitConcensionaria) ) then
+            
+			UPDATE concensionaria set cuitConcensionaria = nueva_cuitConcensionaria , razonSocial = nueva_razonSocial where cuitConcensionaria = _cuitConcensionaria;
             select 0 into nResultado;
-		else 
+		
+        else 
 			select -1 into nResultado;
-            select "Error al Modificar debido a que esta ingresando el mismo dato" into cMensaje;
+            select "Error al Modificar debido a que esta ingresando uno o mas datos repetidos" into cMensaje;
 		end if;
     else
 		select -2 into nResultado;
