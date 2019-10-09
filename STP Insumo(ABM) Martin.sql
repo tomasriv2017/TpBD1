54db0e3d99c9c86f8d10285e0c1731abbd04aa02
tomasriv2017 <tomasriv2017@gmail.com>
2019-10-09 14:09:27 -0300

    update (A,B,M) de Insumo y Proveedor

--- STP Insumo(ABM) Martin.sql
+++ STP Insumo(ABM) Martin.sql
@@ -24,30 +24,46 @@ DELIMITER $$
 CREATE PROCEDURE eliminarInsumo( in _idInsumo int ,out cMensaje varchar(100) , out nResultado int)
 BEGIN
 	declare cantidadRepetida int default 0;
-select count(*) into cantidadRepetida from insumo where idInsumo = _idInsumo;
-   
+    declare cantidadProveedor int default 0;
+    select count(*) into cantidadRepetida from insumo where descripcionInsumo = _descripcionInsumo;
+    select count(*) into cantidadProveedor from insumo_provedor where Insumo_idInsumo = _idInsumo;
+    
     if(cantidadRepetida > 0) then
-	delete from insumo where idInsumo = _idInsumo;
-    	else
-		select -1 into nResultado;
-	select "El Insumo que quiere eliminar no existe" into cMensaje;
-     	end if;
+		if(cantidadProveedor > 0) then
+			select -1 into nResultado;
+            select "No Se Puede eliminar el insumo debido a que lo tiene un proveedor" into cMensaje;
+		else
+			select 0 into nResultado;
+            delete from insumo where idInsumo = _idInsumo;
+		end if;
+	else
+		select -2 into nResultado;
+        select "El Insumo que quiere eliminar no existe" into cMensaje;
+	end if;
 END$$
 DELIMITER ;
 
 /*MODIFICACION*/
 DELIMITER $$
-CREATE PROCEDURE modificarInsumo( in _idInsumo int, in _descripcionInsumo varchar(45), in _precio double ,out cMensaje varchar(100) , out nResultado int)
+CREATE PROCEDURE modificarInsumo( in _idInsumo int, in nueva_descripcionInsumo varchar(45), in nuevo_precio double , out cMensaje varchar(100) , out nResultado int)
 BEGIN
-	declare cantidadRepetida int default 0;
-select count(*) into cantidadRepetida from insumo where idInsumo = _idInsumo;
-   
+	declare cantidadReeptida int default 0;
+	select count(*) into cantidadRepetida from insumo where idInsumo = _idInsumo;
+    
     if(cantidadRepetida > 0) then
-	UPDATE insumo set descripcionInsumo = _descripcionInsumo , precio = _precio 
-        where idInsumo = _idInsumo;
-    	else
-		select -1 into nResultado;
-	select "El Insumo que quiere eliminar no existe" into cMensaje;
-     	end if;
+		if( nueva_descripcionInsumo <> (select descripcionInsumo from insumo where idInsumo = _idInsumo) and
+			nuevo_precio <> (select precio from insumo where idInsumo = _idInsumo) ) then
+			
+            update insumo set descripcionInsumo = nueva_descripcionInsumo , precio = nuevo_precio where idInsumo = _idInsumo;
+            select 0 into nResultado;
+		else
+			select -1 into nResultado;
+			select "No Se Puede Modificar debido a que se repiten uno o mas datos" into cMensaje;
+		end if;
+	
+    else
+		select -2 into nResultado;
+        select "El Insumo que quiere modificar no Existe" into cMensaje;
+	end if;
 END$$
 DELIMITER ;
--- STP Proveedor(ABM).sql
+++ STP Proveedor(ABM).sql
@@ -2,26 +2,69 @@
 
 /*ALTA*/
 DELIMITER $$
-CREATE PROCEDURE agregarProveedor ( in _cuitProvedor varchar(45), in _razonSocial varchar(45) )
+CREATE PROCEDURE agregarProveedor ( in _cuitProvedor varchar(45), in _razonSocial varchar(45), out cMensaje varchar(100) , out nResultado int )
 BEGIN
-	insert into provedor values(_cuitProvedor,_razonSocial);
+		declare cantidadRepetida int default 0;
+    select count(*) into cantidadRepetida from provedor where cuitProvedor = _cuitProvedor;
+    
+     if (cantidadRepetida > 0 ) then
+		select "El Proveedor ya existe" into cMensaje;
+        select -1 into nResultado;
+	else
+		insert into provedor values(_cuitProvedor,_razonSocial);
+		select 0 into nResultado;
+    end if;
 END$$
 DELIMITER ;
 
 
 /*BAJA*/
 DELIMITER $$
-CREATE PROCEDURE eliminarProvedor( in _cuitProvedor varchar(45) )
+CREATE PROCEDURE eliminarProvedor( in _cuitProvedor varchar(45) , out cMensaje varchar(100) , out nResultado int )
 BEGIN
-	delete from provedor where cuitProvedor = _cuitProvedor;
+	declare cantidadRepetida int default 0;
+    declare cantidadInsumo int default 0;
+    select count(*) into cantidadRepetida from provedor where cuitProvedor = _cuitProvedor;
+    select count(*) into cantidadInsumo from insumo_provedor where Provedor_cuitProvedor = _cuitProvedor;
+    
+    if(cantidadRepetida > 0) then
+		if(cantidadInsumo > 0) then
+			select -1 into nResultado;
+            select "No Se Puede eliminar el proveedor debido a que tiene uno o mas insumos" into cMensaje;
+		else
+			select 0 into nResultado;
+            delete from provedor where cuitProvedor = _cuitProvedor;
+		end if;
+	else
+		select -2 into nResultado;
+        select "El Proveedor que quiere eliminar no existe" into cMensaje;
+	end if;
 END$$
 DELIMITER ;
 
 
 /*MODIFICACION*/
 DELIMITER $$
-CREATE PROCEDURE modificarProvedor( in _cuitProvedor varchar(45), in _razonSocial varchar(45) )
+CREATE PROCEDURE modificarProvedor( in _cuitProvedor varchar(45), in nueva_cuitProvedor varchar(45) ,in nueva_razonSocial varchar(45) , out cMensaje varchar(45) , out nResultado int)
 BEGIN
-		UPDATE provedor set razonSocial = _razonSocial where cuitProvedor = _cuitProvedor;
+	declare cantidadRepetida int default 0;
+    select count(*) into cantidadRepetida from provedor where cuitProvedor = _cuitProvedor;
+    
+    if(cantidadRepetida > 0) then
+		if( nueva_cuitProvedor <> (select cuitProvedor from provedor where cuitProvedor = _cuitProvedor) 
+			and nueva_razonSocial <> (select razonSocial from provedor where cuitProvedor = _cuitProvedor) ) then
+            
+			UPDATE provedor set cuitProvedor = nueva_cuitProvedor , razonSocial = nueva_razonSocial where cuitProvedor = _cuitProvedor;
+            select 0 into nResultado;
+		
+        else 
+			select -1 into nResultado;
+            select "Error al Modificar debido a que esta ingresando uno o mas datos repetidos" into cMensaje;
+		end if;
+    else
+		select -2 into nResultado;
+		select "El Proveedor que quiere modificar no existe" into cMensaje;
+	
+    end if;
 END$$
 DELIMITER ;
